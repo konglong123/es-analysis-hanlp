d540a91ba38b878732e0633c84383b14be7b2deb
konglong123 <17888821571@163.com>
2020-04-17 13:08:58 +0800

    word2vec改进

--- src/main/java/com/hankcs/hanlp/mining/word2vec/Word2VecTrainer.java
+++ src/main/java/com/hankcs/hanlp/mining/word2vec/Word2VecTrainer.java
@@ -4,6 +4,7 @@ package com.hankcs.hanlp.mining.word2vec;
 import com.hankcs.hanlp.utility.TextUtility;
 
 import java.io.IOException;
+import java.util.Map;
 
 import static com.hankcs.hanlp.utility.Predefine.logger;
 
@@ -157,18 +158,8 @@ public class Word2VecTrainer
         return this;
     }
 
-
-    /**
-     * 执行训练
-     *
-     * @param trainFileName     输入语料文件
-     * @param modelFileName     输出模型路径
-     * @return 词向量模型
-     */
-    public WordVectorModel train(String trainFileName, String modelFileName)
-    {
+    public Config initConfig(){
         Config settings = new Config();
-        settings.setInputFile(trainFileName);
         settings.setLayer1Size(layerSize);
         settings.setUseContinuousBagOfWords(type == NeuralNetworkType.CBOW);
         settings.setUseHierarchicalSoftmax(useHierarchicalSoftmax);
@@ -179,52 +170,36 @@ public class Word2VecTrainer
         settings.setWindow(windowSize);
         settings.setIter(iterations);
         settings.setMinCount(minFrequency);
+        return settings;
+    }
+
+    public WordVectorModel train(String trainFileName, String modelFileName, Config settings){
+        if (settings==null)
+            settings=initConfig();
+        settings.setInputFile(trainFileName);
         settings.setOutputFile(modelFileName);
         Word2VecTraining model = new Word2VecTraining(settings);
         final long timeStart = System.currentTimeMillis();
-//        if (callback == null)
-//        {
-//            callback = new TrainingCallback()
-//            {
-//                public void corpusLoading(float percent)
-//                {
-//                    System.out.printf("\r加载训练语料：%.2f%%", percent);
-//                }
-//
-//                public void corpusLoaded(int vocWords, int trainWords, int totalWords)
-//                {
-//                    System.out.println();
-//                    System.out.printf("词表大小：%d\n", vocWords);
-//                    System.out.printf("训练词数：%d\n", trainWords);
-//                    System.out.printf("语料词数：%d\n", totalWords);
-//                }
-//
-//                public void training(float alpha, float progress)
-//                {
-//                    System.out.printf("\r学习率：%.6f  进度：%.2f%%", alpha, progress);
-//                    long timeNow = System.currentTimeMillis();
-//                    long costTime = timeNow - timeStart + 1;
-//                    progress /= 100;
-//                    String etd = Utility.humanTime((long) (costTime / progress * (1.f - progress)));
-//                    if (etd.length() > 0) System.out.printf("  剩余时间：%s", etd);
-//                    System.out.flush();
-//                }
-//            };
-//        }
-        settings.setCallback(callback);
-
-        try
-        {
+        try {
             model.trainModel();
             System.out.println();
             System.out.printf("训练结束，一共耗时：%s\n", Utility.humanTime(System.currentTimeMillis() - timeStart));
             return new WordVectorModel(modelFileName);
-        }
-        catch (IOException e)
-        {
+        } catch (IOException e) {
             logger.warning("训练过程中发生IO异常\n" + TextUtility.exceptionToString(e));
         }
-
         return null;
+    }
+
+    /**
+     * 执行训练
+     *
+     * @param trainFileName     输入语料文件
+     * @param modelFileName     输出模型路径
+     * @return 词向量模型
+     */
+    public WordVectorModel train(String trainFileName, String modelFileName)
+    {
+        return train(trainFileName,modelFileName,null);
     }
 }
